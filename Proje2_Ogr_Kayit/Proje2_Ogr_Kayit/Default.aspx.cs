51cd8079e579b4d882ff64d769b24e8fa07b519d
MEHMET <64442588+mhmtlger12@users.noreply.github.com>
2020-10-09 12:50:25 +0300

    Son

---
+++ Proje2_Ogr_Kayit/Proje2_Ogr_Kayit/Default.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Proje2_Ogr_Kayit
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            DataSet1TableAdapters.TBLOGRENCILERTableAdapter dt = new DataSet1TableAdapters.TBLOGRENCILERTableAdapter();
+            dt.OgrencıEkle(TxtAdSoyad.Text, TxtTelefon.Text, TxtMail.Text, DropDownList1.SelectedItem.ToString(), DropDownList2.SelectedItem.ToString());
+        }
+    }
+}
